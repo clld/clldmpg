cb1db93ff48b8cad90238862a668f19d2993198f
xrotwang <xrotwang@googlemail.com>
2016-01-07 21:36:51 +0100

    bumbed version number

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ testing_extras = tests_require + [
 
 setup(
     name='clldmpg',
-    version='1.1.1',
+    version='2.0.0',
     description=(
         'Python library supporting development of CLLD apps maintained by MPI SHH'),
     long_description='',
