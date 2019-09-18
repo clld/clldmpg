7bd6fb9ba898f52c7686adb0ceb71f9aadfaa620
xrotwang <xrotwang@googlemail.com>
2019-09-18 08:23:14 +0200

    bumped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clldmpg',
-    version='3.4.0',
+    version='3.4.1.dev0',
     description=(
         'Python library supporting development of CLLD apps maintained by MPI SHH'),
     long_description='',
